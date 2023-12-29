92d756a55217dcb3de5749829a5cafb23e5f36d2
Niksimas <115989137+Niksimas@users.noreply.github.com>
2023-12-30 00:26:27 +0700

    Add files via upload

--- RGR/controller.h
+++ RGR/controller.h
@@ -1,40 +1,74 @@
 #include <iostream>
-#include "office.h"
+#include "orders.h"
 using namespace std;
 
 class controller {
 private:
-    string name;
-    manager_office_development office_development;
+    cleaning_service current_service;
 public:
     controller() {
-        name="default";
     }
     void execute() {
-        while (true) {
-            office_development.loading_from_file();
+        int run = 1;
+        while (run) {
             menu();
-            int num_comm = 0;
-            cout << "Input number command: \n>>>";
-            cin >> num_comm;
-            if (num_comm == 1) {office_development.print();}
-            else if (num_comm == 2) {office_development.add_new_record();}
-            else if (num_comm == 3) {
-                office_development.print();
-                office_development.deleted_record();
+            int var = 0;
+            while (var < 1 || var > 8) {
+                cout << "Input variant: ";
+                cin >> var;
             }
-            else if (num_comm == 4) {office_development.edit_record();}
-            else if (num_comm == 5) {office_development.save_to_file(); break;}
-            else cout << "Invalid number command"; num_comm =0;
+            if (var == 8) {
+                run = 0;
+            }
+            if (var == 1) {
+                current_service.print();
+            }
+            if (var == 2) {
+                current_service.addRc();
+                cout << "Заказ успешно создан!";
+            }
+            if (var == 3) {
+                current_service.print();
+                cout << "Введите номер заказа, которой надо удалить:\n";
+                int n;
+                cin >> n;
+                current_service.deleteRc(n);
+            }
+            if (var == 4) {
+                current_service.saveToFile(true);
+                cout << "Заказы успешно сохранены!" << endl;
+            }
+            if (var == 5) {
+                current_service.saveToFile(false);
+            }
+            if (var == 6) {
+                current_service.loadFile();
+                cout << "Заказы из файла успешно загружены!" << endl;
+            }
+            if (var == 7) {
+                cout << "Выберите заказ, который следует отредактировать:" << endl;
+                int choose= 0;
+                while (choose < 1 || choose>current_service.getN() + 1) {
+                    current_service.print();
+                    cout << "[" << current_service.getN() + 1 << "] " << "Отмена." << endl;
+                    cin >> choose;
+
+                }
+                current_service.changeRecord(choose);
+                cout << "Заказ успешно отредактирован!" << endl;
+            }
         }
     }
 
     void menu() {
-        cout << "Commands: \n"
-        << "1) Print all record\n"
-        << "2) Creat new record\n"
-        << "3) Deleted record\n"
-        << "4) Edit record\n"
-        << "5) Save and exit\n";
+        cout << "Commands: " << endl;
+        cout << "[1] Вывести все заказы" << endl;
+        cout << "[2] Создать новую запись о заказе" << endl;
+        cout << "[3] Удалить запись о заказе" << endl;
+        cout << "[4] Добавить текущие заказы в файл" << endl;
+        cout << "[5] Перазаписать текущий файл с заказами" << endl;
+        cout << "[6] Загрузить заказы из файла" << endl;
+        cout << "[7] Отредактировать заказ" << endl;
+        cout << "[8] Выход из программы" << endl;
     }
 };
---
+++ RGR/record.h
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "people.h"
+#include <time.h>
+using namespace std;
+
+class record {
+private:
+    string object;
+    float S;
+    string clean;
+    people client;
+    string type_object[9] = {
+        "Квартира","Коттедж","Офис",
+        "Магазин","Торговый центр","Производство",
+        "Склад","Многоквартирный дом","Другое"
+    };
+    string type_clean[10] = {
+        "Разовая","Ежемесячная","Химчистка",
+        "Уборка после ремонта","Уборка после ЧП","Удаление запахов",
+        "Удаление плесени","Промышленный альпинизм","Дезинфекция",
+        "Другое"
+    };
+public:
+    record() {
+        object = "Квартира";
+        S = 40.0;
+        clean = "Разовая";
+    }
+    record(string name,string surname,string number, string object, float S, string clean) {
+        this->client = *(new people(name, surname, number));
+        this->S = S;
+        this->clean = clean;
+    }
+    string getObject() {
+        return object;
+    }
+    float getS() {
+        return S;
+    }
+    string getClean() {
+        return clean;
+    }
+    people getClient() {
+        return client;
+    }
+    string getClientInfo() {
+        client.print();
+    }
+    void print() {
+        client.print();
+        cout << "Тип объекта: " << object << " - " << S <<" кв.м" << "; Тип уборки: " << clean << endl;
+    }
+    void change() {
+        cout << "Выберите пункт заказа, который вы хотели бы отредактировать:\n";
+        cout << "1 - Объект\n";
+        cout << "2 - Площадь объекта\n";
+        cout << "3 - Тип уборки\n";
+        cout << "4 - Информацию о клиенте\n";
+        int choose = 0;
+        while (choose < 1 || choose>4){
+            cin >> choose;
+        }
+        switch (choose){
+        case 1:
+            {
+            cout << "Выберите тип объекта:" << endl;
+            int select = 0;
+            for (int i = 0; i < 9; i++) {
+                cout << "[" << i + 1 << "] " << type_object[i] << endl;
+            }
+            while (select < 1 || select>9) {
+                cin >> select;
+            }
+            if (select == 9) {
+                cout << "Введите свой тип объекта: ";
+                cin >> object;
+            }
+            else object = type_object[select - 1];
+            break;}
+        case 2:
+        {
+            cout << "Укажите площадь (кв.м.) объекта: ";
+            cin >> S;
+            break;
+        }
+        case 3:
+        {
+            int select = 0;
+            cout << "Выберите тип уборки:" << endl;
+            for (int i = 0; i < 10; i++) {
+                cout << "[" << i + 1 << "] " << type_clean[i] << endl;
+            }
+            while (select < 1 || select>10) {
+                cin >> select;
+            }
+            if (select == 10) {
+                cout << "Введите нужный вам тип уборки: ";
+                cin >> clean;
+            }
+            else clean = type_clean[select - 1];
+            break;
+        }
+        case 4:
+        {
+            string name, surname, number;
+            cout << "Укажите имя нового клиента: ";
+            cin >> name;
+            cout << "Укажите фамилию нового клиента: ";
+            cin >> name;
+            cout << "Укажите номер телефона нового клиента для дальнейшей связи: ";
+            cin >> number;
+            client = people(name, surname, number);
+            break;
+        }
+        default:
+            break;
+        }
+    }
+};
